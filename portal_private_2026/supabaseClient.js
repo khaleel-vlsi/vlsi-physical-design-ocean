dce71c4f10ca5ce59148542c1051f9df6c462de8
khaleel-vlsi <vlsiphysicaldesignocean@gmail.com>
2026-02-20 07:08:54 +0530

    Update Supabase key to use ANON JWT

--- portal_private_2026/supabaseClient.js
+++ portal_private_2026/supabaseClient.js
@@ -1,8 +1,9 @@
 // portal_private_2026/supabaseClient.js
-// Supabase client (browser) using CDN build
 
 const SUPABASE_URL = "https://ygcvcoynmyrplwrpisd.supabase.co";
-const SUPABASE_KEY = "sb_publishable_ZMuHJtseBcfEUwnoybQQ0A_uau7W_6j";
 
-// Create client
+// ✅ Use ANON key (JWT)
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnY3ZjeW95bm15cnBsd3JwaXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NDg4NzUsImV4cCI6MjA4NzAyNDg3NX0.pfeo4p42y53fCaA49oe1yXXFU22BvTEotzlZAFhYzqU";
+
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);

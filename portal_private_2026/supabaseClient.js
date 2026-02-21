b02ff4ead2253840a3a6f520e2ce4e32b483ad10
khaleel-vlsi <vlsiphysicaldesignocean@gmail.com>
2026-02-21 06:24:34 +0530

    Update Supabase URL in supabaseClient.js

--- portal_private_2026/supabaseClient.js
+++ portal_private_2026/supabaseClient.js
@@ -1,6 +1,6 @@
 // portal_private_2026/supabaseClient.js
 
-const SUPABASE_URL = "https://ygcvcoynmyrplwrpisd.supabase.co";
+const SUPABASE_URL = "https://ygcvcyoynmyrplwrpisd.supabase.co";
 
 // ✅ Use ANON key (JWT)
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnY3ZjeW95bm15cnBsd3JwaXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NDg4NzUsImV4cCI6MjA4NzAyNDg3NX0.pfeo4p42y53fCaA49oe1yXXFU22BvTEotzlZAFhYzqU";

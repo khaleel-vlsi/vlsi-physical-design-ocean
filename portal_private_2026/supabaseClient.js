ef4b1d045b943052046542e35df9fda9620e7e99
khaleel-vlsi <vlsiphysicaldesignocean@gmail.com>
2026-02-21 06:38:42 +0530

    Rename supabase variable to supabaseClient

--- portal_private_2026/supabaseClient.js
+++ portal_private_2026/supabaseClient.js
@@ -5,4 +5,4 @@ const SUPABASE_URL = "https://ygcvcyoynmyrplwrpisd.supabase.co";
 // ✅ Use ANON key (JWT)
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnY3ZjeW95bm15cnBsd3JwaXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NDg4NzUsImV4cCI6MjA4NzAyNDg3NX0.pfeo4p42y53fCaA49oe1yXXFU22BvTEotzlZAFhYzqU";
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
